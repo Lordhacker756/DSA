f6f66da9aefc085f2afbc85ee930f2b1231b3073
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2022-05-13 21:27:30 +0530

    Added Flow of program

---
+++ Fundamentals/Flow Of Programs.java
@@ -0,0 +1,11 @@
+/*
+
+FLOWCHART: used to vizualise a complex program
+🔶 Start-Stop: Circle/Oval⭕
+🔶 Input-Output: Paralellogram
+🔶 Processing: Rectangle ⏹️
+🔶 Conditions: Square🔷 (Sides equally inclined to VP😜)
+
+PSEUDO CODE
+Using the jargon of coding but writing in plane english in the flow like a flowchart
+*/
